5787c0dabdf0d0a866406ce555af900590f371ed
Серафим <seraphim.uhanov@gmail.com>
2023-10-19 01:57:24 +0300

    fix

--- main-service/src/main/java/ru/practicum/main/service/location/repository/LocationRepository.java
+++ main-service/src/main/java/ru/practicum/main/service/location/repository/LocationRepository.java
@@ -1,10 +1,12 @@
 package ru.practicum.main.service.location.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.main.service.location.model.Location;
 
 import java.util.Optional;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Optional<Location> findByLatAndLon(float lat, float lon);
 }
--- main-service/src/main/java/ru/practicum/main/service/request/repository/api/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/main/service/request/repository/api/RequestRepository.java
@@ -1,11 +1,13 @@
 package ru.practicum.main.service.request.repository.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.main.service.request.model.ParticipationRequest;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RequestRepository extends JpaRepository<ParticipationRequest, Long> {
     Optional<List<ParticipationRequest>> findAllByRequesterId(Long requesterId);
 
